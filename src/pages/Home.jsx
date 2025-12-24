feefd5c818ffab5c825c7efd4aeb93998d92bcd9
rahulya43 <veerarahulyadav_ankala@srmap.edu.in>
2025-12-24 11:30:04 +0530

    Implement Navbar component with responsive navigation

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export const Home = ()=>{
+    return <div>
+
+    {/* Theme Toggle*/}
+
+    {/* BackGround Effects*/}
+
+    {/* NavBar*/}
+
+
+    {/* Main Content*/}
+
+    {/* Footer*/}
+
+
+    </div>
+}
