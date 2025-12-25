32baa6729a9b11596cb34fd5243be5fcce9733ca
rahulya43 <veerarahulyadav_ankala@srmap.edu.in>
2025-12-25 10:30:08 +0530

    Add UI components, hooks, and project assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,34 @@
-export const Home = ()=>{
-    return <div>
+import { Navbar } from "../components/Navbar";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectSection } from "../components/ProjectSection";
+import { ContactSection } from "../components/ContactSection";
+import  {Footer } from "../components/Footer";
 
-    {/* Theme Toggle*/}
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
 
-    {/* BackGround Effects*/}
+      {/* Navbar */}
+      <Navbar />
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectSection />
+        <ContactSection />
+      </main>
 
-    {/* NavBar*/}
-
-
-    {/* Main Content*/}
-
-    {/* Footer*/}
-
-
+      {/* Footer */}
+      <Footer />
     </div>
-}
+  );
+};
